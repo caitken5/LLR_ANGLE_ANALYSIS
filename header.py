cafc4ca5125616980aad368493fcefa1a224a676
sobriqu83 <caitken5@uwo.ca>
2022-09-04 19:22:59 -0400

    Added new calculation for reaction time using both force and velocity, which does vary mildly sometimes. This will allow for a calculation of the error in a particular movement. We start with their initial movement, and then end once the target is reached. Added some plotting capabilities as well. Changed angle calculation to degrees from rad for ease of interpretation.

--- header.py
+++ header.py
@@ -70,7 +70,9 @@ def angle_between_vectors(vector_1, vector_2):
     for i in range(len_1):
         point_1 = vector_1[i, :]
         point_2 = vector_2[i, :]
-        theta_arr[i] = angle_between_points(point_1, point_2)
+        theta_arr[i] = angle_between_points(point_1, point_2)*180/np.pi
+        # Make sure all values are converted to degrees because it's
+        # easier to make sense in the brain what is happening!
     return theta_arr
 
 
@@ -99,16 +101,31 @@ def butterworth_filter(data, cutoff, fs):
     return filtered_data
 
 
-def get_vel_min(vel):
+def get_min(data):
     # This function takes in the velocity data and calculates whether the first minimum in the data has been achieved.
     # Argrelmin requires a strict minimum, ie. increasing on both sides. Since find_peaks can detect lows that are not
     # strict minimums, I will use that.
     my_min = None  # Base set so I can deal with occasions that don't fit what I expect.
     first_min = []
     # Multiply my series by -1 to find the maxes (which are actually the minima!)
-    neg_vel = -1*vel
+    neg_vel = -1*data
     my_min_series = signal.find_peaks(neg_vel)[0]  # Use 0 to extract the array.
-    # TODO: Account for when my_min_series has no minimums.
     if my_min_series.size != 0:
         my_min = my_min_series[0]
     return my_min
+
+
+def get_angle_error(data):
+    # Retrieve the mean of the error.
+    return np.mean(data)
+
+
+def get_50(data, my_max):
+    my_50 = 0.5 * (data[my_max] - data[0]) + data[0]
+    index_50 = None
+    # Loop through and find the first sample
+    for i in range(data.shape[0]):
+        if data[i] >= my_50:
+            index_50 = i
+            break
+    return index_50
--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 # This code will be used for producing the difference in angle of average for Force/Velocity Direction Error.
 # It will also be used to show the variability in the directed angle. Look for ways to describe the variability of the signal. Frequency analysis?
 
-# TODO: Determine if this should be done for the entire sequence, or if this should be done for only after the reaction
-#  time (that code can be fed in to demonstrate what is happening). I think after reaction time makes sense.
 # TODO: Generate function that calculates average error in this applied force/velocity.
-
 # TODO: Given this angle of error, apply smoothness function done in Google Colab to ensure that there is no jump in
 #  the data that would throw off the calculation of variability.
-# TODO: Find metric that demonstrates variability of a signal. Due to small samples,
-#  performing frequency analysis on this would be difficult.
-
-# TODO: May want to insert the code for the calculation of the angle difference for both force and velocity into the
-#  data cleaning code, since this can be treated as an additional signal.
+# TODO: Find metric that demonstrates variability of a signal.
 
 import os
 import numpy as np
@@ -25,11 +18,11 @@ source_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCE
                 "4_LLR_DATA_SEGMENTATION/NPZ_FILES_BY_TARGET"
 error_angle_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/8_LLR_ANGLE_GRAPHS/" \
                      "ERROR_ANGLE"
-reaction_and_end_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/" \
-                          "8_LLR_ANGLE_GRAPHS/REACTION_AND_END"
+react_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/8_LLR_ANGLE_GRAPHS/" \
+                     "REACTION_TIME"
 
 plot_error_angle = True
-plot_react_and_min = True
+plot_react = True
 save_graphs = True
 
 if __name__ == '__main__':
@@ -42,7 +35,7 @@ if __name__ == '__main__':
             if (("T1" in file) or ("T2" in file)) & ("V0" in file):
                 # Here if file includes task 1 and 2, and if it also is a V0 file, as I'm not studying other pieces
                 # of information.
-                # print(file)
+                print(file)
                 source_file = source_folder + '/' + file
                 file_name = file.split('.')[0]
                 # The source file is a npz file. So I need to load in the data and unpack.
@@ -61,6 +54,8 @@ if __name__ == '__main__':
                     t = stuff[:, h.data_header.index("Time")]
                     force = stuff[:, [h.data_header.index("CorrForce_X"), h.data_header.index("CorrForce_Y")]]
                     velocity = stuff[:, [h.data_header.index("X_Vel"), h.data_header.index("Y_Vel")]]
+                    vel = stuff[:, h.data_header.index("Vxy_Mag")]
+                    forse = stuff[:, h.data_header.index("Fxy_Mag")]
                     des_pos = stuff[:, [h.data_header.index("Des_X_Pos"), h.data_header.index("Des_Y_Pos")]]
                     f_abs_angle = stuff[:, h.data_header.index("Fxy_Angle")]
                     v_abs_angle = stuff[:, h.data_header.index("Vxy_Angle")]
@@ -81,17 +76,20 @@ if __name__ == '__main__':
                         # Plot the thetas!
                         fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
                         fig.set_size_inches(25, 8)
+                        plt.suptitle("Error Angles for Force and Velocity " + file_name + "_" + target_num + "_" + to_from_home + "_" + num_prev)
                         ax1 = fig.add_subplot(211)
                         ax2 = fig.add_subplot(212)
                         ax1.grid(visible=True)
+                        ax1.set_title("Force")
                         ax1.plot(t, force_theta, label="Force Target Error")
                         ax1.plot(t, f_abs_angle, label="Absolute Force")
+                        ax2.set_title("Velocity")
                         ax2.plot(t, velocity_theta, label="Velocity Target Error")
                         ax2.plot(t, v_abs_angle, label="Absolute Velocity")
-                        ax1.set_ylabel("Angle [rad]")
+                        ax1.set_ylabel("Angle [degrees]")
                         ax1.legend()
                         ax2.set_xlabel("Time [s]")
-                        ax2.set_ylabel("Angle [rad]")
+                        ax2.set_ylabel("Angle [degrees]")
                         ax2.legend()
                         save_str = error_angle_folder + '/' + file_name + "_TARGET-" + target_num + "_HOME-" + to_from_home + "_PREV-" + num_prev
                         if save_graphs:
@@ -100,51 +98,69 @@ if __name__ == '__main__':
                             gc.collect()
                         else:
                             plt.show()
+                            plt.close()
                     # Get reaction time index.
                     reaction_time_index = h.reaction_time_index(dist_from_target)
+                    v_min_index = h.get_min(vel)
+                    f_min_index = h.get_min(forse)
+                    # If the minimum of each of these occurs, then get the maximum force value that occurs after this
+                    # minimum, and find the first time at which the signal reaches 50% of the maximum value.
+                    f_50 = None
+                    v_50 = None
+                    if v_min_index is not None:
+                        v_after = vel[v_min_index:]
+                        v_max = np.argmax(v_after)
+                        v_50 = h.get_50(v_after, v_max) + v_min_index
+                    if f_min_index is not None:
+                        f_after = forse[f_min_index:]
+                        f_max = np.argmax(f_after)
+                        f_50 = h.get_50(f_after, f_max) + f_min_index
                     # Filter the velocity with very low filter.
                     if reaction_time_index is None:
-                        print("Could not calculate reaction time for file " + file_name + "for target " +  target_num + ", to home is " + to_from_home + ", num prev is " + num_prev)
+                        print("Could not calculate reaction time for file " + file_name + "for target " + target_num + ", to home is " + to_from_home + ", num prev is " + num_prev)
                     else:
                         # Take the section of data that corresponds to everything after the reaction time.
                         reaction_stuff = stuff[reaction_time_index:, :]
                         # NOTE: Remember when transferring row information from reaction_stuff to stuff that it will be
                         # translated by reaction_time_index.
-                        vel = stuff[:, h.data_header.index("Vxy_Mag")]
-                        Vxy_Mag = reaction_stuff[:, h.data_header.index("Vxy_Mag")]
-                        vf = h.butterworth_filter(Vxy_Mag, 3, 100)
-                        vel_min_1 = h.get_vel_min(vf)
-                        if vel_min_1 is None:
-                            print("Could not calculate end of first movement for file " + file_name + "for target "
-                                  + target_num + ", to home is " + to_from_home + ", num prev is " + num_prev)
-                        else:
-                            vel_min_1 = h.get_vel_min(vf) + reaction_time_index
-                            if plot_react_and_min:
-                                # Plot the reaction time and when the first minimum occurs. Can be used to filter out samples
-                                # that don't fit the desired trend and might be outlier data.
-                                fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
-                                fig.set_size_inches(25, 8)
-                                plt.suptitle("Velocity, Reaction Time, and End of First Movement for " + file_name)
-                                ax1 = fig.add_subplot(111)
-                                ax1.grid(visible=True)
-                                ax1.plot(t, vel, label="Velocity [m/s]")
-                                ax1.axvline(x=t[reaction_time_index], color='g', label="Reaction Time")
-                                ax1.axvline(x=t[vel_min_1], color='r', label="First Minimum")
-                                ax1.set_xlabel("Time [s]")
-                                ax1.legend()
-                                # Add some stuff to save the file here.
-                                save_str = reaction_and_end_folder + '/' + file_name + "_TARGET-" + target_num + "_HOME-" + to_from_home + "_PREV-" + num_prev
-                                if save_graphs:
-                                    plt.savefig(fname=save_str)
-                                    fig.clf()
-                                    gc.collect()
-                                else:
-                                    plt.show()
-                                    plt.close()
-                            # TODO: Calculate error of angle for force and velocity from reaction time until end.
+                        if plot_react:
+                            vel = stuff[:, h.data_header.index("Vxy_Mag")]
+                            force = stuff[:, h.data_header.index("Fxy_Mag")]
+                            # Plot the reaction time and when the first minimum occurs. Can be used to filter out samples
+                            # that don't fit the desired trend and might be outlier data.
+                            fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
+                            fig.set_size_inches(25, 8)
+                            plt.suptitle("Force and Velocity Reaction Times for " + file_name + "_" + target_num + "_" + to_from_home + "_" + num_prev)
+                            ax1 = fig.add_subplot(111)
+                            ax1.grid(visible=True)
+                            ax1.plot(t, vel*100, label="Velocity [cm/s]")
+                            ax1.plot(t, force, label="Force [N]")
+                            # Reaction time from Olivia's work does not appear to work as well as liked.
+                            if v_min_index is not None:
+                                ax1.axvline(x=t[v_min_index], color='g', label="First Minimum Velocity")
+                            if v_50 is not None:
+                                ax1.axvline(x=t[v_50], color='g', label="Velocity Reaction Time")
+                            if f_min_index is not None:
+                                ax1.axvline(x=t[f_min_index], color='r', label="First Minimum Force")
+                            if f_50 is not None:
+                                ax1.axvline(x=t[f_50], color='r', label="Force Reaction Time")
+                            ax1.set_xlabel("Time [s]")
+                            ax1.set_ylabel("Magnitude")
+                            ax1.legend()
+                            # Add some stuff to save the file here.
+                            save_str = react_folder + '/' + file_name + "_TARGET-" + target_num + "_HOME-" + to_from_home + "_PREV-" + num_prev
+                            if save_graphs:
+                                plt.savefig(fname=save_str)
+                                fig.clf()
+                                gc.collect()
+                            else:
+                                plt.show()
+                                plt.close()
+                            # Calculate the mean of the force angle and the velocity angle.
+                            mean_force_error = h.get_angle_error(force_theta[reaction_time_index:])
+                            mean_velocity_error = h.get_angle_error(velocity_theta[reaction_time_index:])
 
                             # Due to the same problem I was having before with the way the task was designed,
                             # I will have trouble identifying end of first movements since I cannot guarantee
                             # that someone has stopped before they have hit the target. For this reason, I will only
                             # take the total error.
-                            # TODO: Calculate error of angle for force and velocity from reaction time to end of first movement.
